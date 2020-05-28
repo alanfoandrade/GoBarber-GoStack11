e3de5bd762b903a6376517001c94d46180a2c5a8
Alan Andrade <alaninatel@gmail.com>
2020-05-28 11:21:56 -0300

    Iniciando front web

---
+++ gobarber-web/src/components/Input/index.tsx
@@ -0,0 +1,39 @@
+import React, { InputHTMLAttributes, useEffect, useRef, useState } from 'react';
+import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
+
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
---
+++ gobarber-web/src/components/Input/styles.ts
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #232129;
+  border-radius: 10px;
+  padding: 16px;
+  width: 100%;
+
+  color: #666360;
+  border: 2px solid #232129;
+
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #f4ede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
